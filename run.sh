791c29d138b57a31f2a887bb706f7db755b0a147
mental1104 <mental1104@gmail.com>
2024-12-22 22:55:24 +0800

    [ADD] 加入公共json基类

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 if [ -n "$1" ]; then
     docker build -t mental1104_dev . --build-arg SSH_PRIVATE_KEY=$1
     docker tag mental1104_dev:latest mental1104/dev:latest
-    docker push mental1104/dev:latest
+    #docker push mental1104/dev:latest
 else
     echo "USAGE: run.sh ssh_passwd"
     exit 1
