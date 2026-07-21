440f7b84a0852b6e1c1d2a8af620eb1ecdc37b71
Blue Espeon <mental1104@gmail.com>
2026-07-21 20:18:52 +0800

    feat(cpp): declare Pulsar message queue adapter

---
+++ cpp/include/mental1104/mq/pulsar.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "mental1104/mq/abstract_message_queue.h"
+
+#include <memory>
+
+namespace mental1104 {
+namespace mq {
+
+bool pulsar_available();
+
+class PulsarMessageQueue : public AbstractMessageQueue {
+public:
+  explicit PulsarMessageQueue(const Options &config = Options());
+  ~PulsarMessageQueue();
+
+  std::shared_ptr<AbstractProducer>
+  create_producer(const std::string &tenant, const std::string &namespace_name,
+                  const std::string &topic, const Schema &schema = Schema(),
+                  bool batching_enabled = true) override;
+
+  std::shared_ptr<AbstractConsumer>
+  create_consumer(const std::string &tenant, const std::string &namespace_name,
+                  const std::string &topic, const std::string &subscription,
+                  const Schema &schema = Schema(),
+                  SubscriptionType subscription_type = SubscriptionType::Shared,
+                  const MessageListener &message_listener = MessageListener(),
+                  const Options &options = Options()) override;
+
+  void close() override;
+
+private:
+  class Impl;
+  std::shared_ptr<Impl> impl_;
+};
+
+} // namespace mq
+} // namespace mental1104
